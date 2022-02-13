cc1a464277675a9985c86b9acfe2c7b1f32ed6e6
Will <WTS012201@gmail.com>
2022-02-12 20:00:23 -0600

    prio queue by cost

--- wts0022_proj1/part1/find_route.cpp
+++ wts0022_proj1/part1/find_route.cpp
@@ -16,61 +16,62 @@ class edge{
     private:
         std::string city1;
         std::string city2;
-        double weight;
+        float weight;
     public:
-        edge(std::string __city1, std::string __city2, double __weight):
+        edge(std::string __city1, std::string __city2, float __weight):
             city1{__city1}, city2{__city2}, weight{__weight}{}
         std::string get_c1() const{  return city1;}
         std::string get_c2() const{  return city2;}
-        double get_weight() const{  return weight;}
+        float get_weight() const{  return weight;}
 };
 
-void load_edges(std::ifstream ifs, std::vector<edge>& edge_set){
+std::vector<edge> load_edges(std::ifstream ifs){
     std::string arg1, arg2, arg3;
-    
+    std::vector<edge> edge_set;
+
     while(std::getline(ifs, arg1, ' ')){
         std::getline(ifs, arg2, ' ');
         std::getline(ifs, arg3);
 
         if(!arg1.compare("END") && !arg2.compare("OF"))
             break;
-        edge_set.push_back(edge{arg1, arg2, std::stod(arg3)});
+        edge_set.push_back(edge{arg1, arg2, std::stof(arg3)});
     }
     ifs.close();
+    return edge_set;
 }
+
+typedef std::pair<std::string, std::string> edge_pair;
 
 class graph {
     public:
-        std::map<std::string, bool> visited;
         std::map<std::string, std::list<std::string>> adj_list;
-        std::map<std::string, std::string> route;
-
-        std::vector<edge> edge_set;
+        std::map<std::string, bool> visited;
+        std::map<std::string, std::string> path;
+        std::map<edge_pair, float> cost;
 
         int expanded;
         int generated;
-        double total_distance;
+        float total_distance;
         
-        graph(std::string infile): expanded{0}, generated{0}{
-            load_edges(std::ifstream{infile}, edge_set);
-            for(const auto e : edge_set)
+        graph(std::string infile): expanded{0}, generated{0}, total_distance{0}{
+            for(const auto& e : load_edges(std::ifstream{infile}))
                 add_edge(e);
         }
 
-        edge* get_edge(std::string city1, std::string city2){
-            for(edge& e : edge_set){
-                if(!e.get_c1().compare(city1) && !e.get_c2().compare(city2))
-                    return &e;
-                if(!e.get_c2().compare(city1) && !e.get_c1().compare(city2))
-                    return &e;
-            }
-            return nullptr;
+        edge_pair* make_edge_pair(std::string city1, std::string city2){
+            return new edge_pair{city1, city2};
         }
 
         void add_edge(edge e){
-            adj_list[e.get_c1()].push_back(e.get_c2());
+            path[e.get_c1()] = "";
+            path[e.get_c2()] = "";
             visited[e.get_c1()] = false;
-            route[e.get_c1()] = "";
+            visited[e.get_c2()] = false;
+            adj_list[e.get_c1()].push_back(e.get_c2());
+            adj_list[e.get_c2()].push_back(e.get_c1());
+            cost[*make_edge_pair(e.get_c1(), e.get_c2())] = e.get_weight();
+            cost[*make_edge_pair(e.get_c2(), e.get_c1())] = e.get_weight();
         }
 
         void print_route(std::string city1, std::string city2){
@@ -81,18 +82,19 @@ class graph {
             std::cout.precision(1);
 
             //  Every node that is visited is generated
+            
             for(const auto& item : visited){
                 if(item.second)
                     generated++;
             }
 
-            while(route[city2].compare("")) {
+            while(path[city2].compare("")) {
                 stack.push(city2);
-                city2 = route[city2];
+                city2 = path[city2];
             }
             
             while(!stack.empty()) {
-                auto edge_distance = get_edge(prev_city, stack.top()) -> get_weight();
+                auto edge_distance = cost[*make_edge_pair(prev_city, stack.top())];
                 total_distance += edge_distance;
                 route_edges.push_back(edge{prev_city, stack.top(), edge_distance});
                 prev_city = stack.top();
@@ -117,43 +119,47 @@ class graph {
                 std::cout << std::fixed << e.get_weight();
                 std::cout << " km" << std::endl;
             }
+            
         }
         
         bool find_route(std::string city1, std::string city2){
-            std::queue<std::string> q;
-            
-            q.push(city1);
+            typedef std::pair<std::string, float> route;
+            auto cmp = [](route lhs, route rhs){
+                return lhs.second > rhs.second;
+            };
+            std::priority_queue<route, std::vector<route>, decltype(cmp)> q(cmp);
+            q.push(route{city1, 0});
             visited[city1] = true;
 
             while(!q.empty()){
-                std::string city = q.front();
-                std::cout << city <<std::endl;
+                route city = q.top();
                 q.pop();
                 expanded += 1;  //  city node gets expanded
-                for(std::string adj_city: adj_list[city]){
+                for(auto adj_city: adj_list[city.first]){
                     if(!visited[adj_city]){
-                        q.push(adj_city);
                         visited[adj_city] = true;
-                        route[adj_city] = city;
+                        auto adj_cost = cost[*make_edge_pair(city.first, adj_city)];
+                        q.push(route{adj_city, adj_cost});
+                        path[adj_city] = city.first;
                         if(!adj_city.compare(city2))
                             return true;
                     }
                 }
             }
+            
             return false;
         }
 };
 
 int main(int argc, char* argv[]){
     if(argc != 4 && argc != 5){
-        std::cout << "Invalid arguments. " << argc << "given\n";
+        std::cout << "Invalid argument amount. " << argc - 1 << "given\n";
         return 1;
     }
 
     std::string origin_city = argv[2];
     std::string destination_city = argv[3];
     std::string heuristic_filename = (argc == 5) ? argv[4] : std::string{};
-
     graph g{argv[1]};
     g.print_route(origin_city, destination_city);
     return 0;
